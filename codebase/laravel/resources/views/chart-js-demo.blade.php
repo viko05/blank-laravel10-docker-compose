2216a08a2c9ee381dbb7c02d72390744dc28d191
viko05 <viktor.kostiuk.0501@gmail.com>
2023-10-05 12:13:09 +0300

    (demo) js assets build   - simple demo usage chart.js

---
+++ codebase/laravel/resources/views/chart-js-demo.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chart js demo</title>
+</head>
+<body>
+<div style="width: 800px;"><canvas id="break-even-chart"></canvas></div>
+
+@vite('resources/js/app.js')
+</body>
+</html>
